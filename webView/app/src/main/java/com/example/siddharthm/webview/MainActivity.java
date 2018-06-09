915a210aec615266dcae640373b778e02da3533f
Siddharth M <siddharth.m98@gmail.com>
2018-06-09 16:18:08 +0530

    Web View

---
+++ webView/app/src/main/java/com/example/siddharthm/webview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.siddharthm.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView)findViewById(R.id.webView);
+        wv.loadUrl("http://www.google.com");
+
+    }
+}
