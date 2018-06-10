70bf529a79372c38ad28bba5cf14890223d1cf43
Siddharth M <siddharth.m98@gmail.com>
2018-06-10 20:56:43 +0530

    update db

--- sqLiteDb/app/src/main/java/com/example/siddharthm/sqlitedb/MainActivity.java
+++ sqLiteDb/app/src/main/java/com/example/siddharthm/sqlitedb/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     dataBaseHelper myDb;
-    EditText ed1,ed2,ed3;
-    Button b,b2;
+    EditText ed1,ed2,ed3,ed4;
+    Button b,b2,b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +23,14 @@ public class MainActivity extends AppCompatActivity {
         ed1 = (EditText)findViewById(R.id.editText);
         ed2 = (EditText)findViewById(R.id.editText2);
         ed3 = (EditText)findViewById(R.id.editText3);
+        ed4 = (EditText)findViewById(R.id.editText4);
         b = (Button)findViewById(R.id.button);
         b2 = (Button)findViewById(R.id.button2);
+        b3 = (Button)findViewById(R.id.button3);
+
         AddData();
         viewAll();
+        updateData();
 
  }
 
@@ -62,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                         buffer.append("ID: " + res.getString(0)+"\n");
                         buffer.append("NAME: " + res.getString(1)+"\n");
                         buffer.append("SURNAME: " + res.getString(2)+"\n");
-                        buffer.append("MARKS: " + res.getString(3)+"\n");
+                        buffer.append("MARKS: " + res.getString(3)+"\n\n");
 
                     }
                     showMessage("DATA",buffer.toString());
@@ -79,4 +83,23 @@ public class MainActivity extends AppCompatActivity {
         builder.setMessage(message);
         builder.show();
     }
+
+    public void updateData(){
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdate =  myDb.UpdateData(ed4.getText().toString(),ed1.getText().toString(),ed2.getText().toString(),ed3.getText().toString());
+                if(isUpdate == true){
+
+                    Toast.makeText(MainActivity.this,"Data updated",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"Data not updated",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+    }
+
 }
