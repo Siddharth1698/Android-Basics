35a2c62fe98193b86fe545982fea757b8716ca99
Siddharth M <siddharth.m98@gmail.com>
2018-06-12 23:08:00 +0530

    Picasso updated

--- picassoCheck/app/src/main/java/com/example/siddharthm/picassocheck/MainActivity.java
+++ picassoCheck/app/src/main/java/com/example/siddharthm/picassocheck/MainActivity.java
@@ -20,35 +20,20 @@ public class MainActivity extends AppCompatActivity {
         i4 = (ImageView)findViewById(R.id.imageView4);
         i5 = (ImageView)findViewById(R.id.imageView5);
 
-        i1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Picasso.get().load("https://www.wpfreeware.com/wp-content/uploads/2014/09/placeholder-images.jpg").into(i1);
-            }
-        });
-        i2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(i2);
-            }
-        });
-        i3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(i3);
-            }
-        });
-        i4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(i4);
-            }
-        });
-        i5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(i5);
-            }
-        });
+
+                Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW3DRLEwttFGnqaBOFeweQ_9EqtR-iZaGPZw94EXDtNE9R7dt8").into(i1);
+
+
+                Picasso.get().load("https://blogs-images.forbes.com/erikkain/files/2017/10/ACO_CurseofPharaoh_Thebes_ScorpionFight_1507597412.jpg").into(i2);
+
+
+                Picasso.get().load("https://static-ca.ebgames.ca/images/products/732184/8scrmax3.jpg").into(i3);
+
+
+                Picasso.get().load("https://cdn.wccftech.com/wp-content/uploads/2017/07/Assassins-Creed-Origins-1.jpg").into(i4);
+
+
+                Picasso.get().load("https://gpstatic.com/acache/33/17/4/uk/s3-622dd3c8a1b0b0ce278efa5c609e44a1.jpg").into(i5);
+
     }
 }
