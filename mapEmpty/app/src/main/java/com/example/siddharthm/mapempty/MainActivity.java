e0872a75c758b7fd38877f35ddee5f53e05ea67c
Siddharth M <siddharth.m98@gmail.com>
2018-06-22 17:00:26 +0530

    Maps Blank Activity with marker

--- mapEmpty/app/src/main/java/com/example/siddharthm/mapempty/MainActivity.java
+++ mapEmpty/app/src/main/java/com/example/siddharthm/mapempty/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.siddharthm.mapsdemo;
-
+package com.example.siddharthm.mapempty;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,15 +11,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(R.layout.activity_main);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -39,6 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
 
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
